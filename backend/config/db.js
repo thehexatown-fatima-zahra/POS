e89f751c325f51781f1f2f94d07d7a012f934136
Fatima Tu Zahra <fatima.zahra@thehexatown.com>
2025-08-12 12:50:25 +0500

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    console.log(` MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error(" MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
